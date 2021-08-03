a28dd880ce83797e305acf257fd048df79ef75d7
ankit.malasiya@gmail.com <Ankit@1989>
2021-08-03 11:12:37 +0200

    spring gateway project

---
+++ spring-gateway/src/test/java/com/ankit/springgateway/SpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.springgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
